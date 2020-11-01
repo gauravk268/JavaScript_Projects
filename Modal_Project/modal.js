c390c50fea3939884faf422d2a00314126dcc72c
gauravk268 <gauravk26800@gmail.com>
2020-11-01 22:24:29 +0530

    - modal function working - updated thumbnail for modal - updated modal content

--- Modal_Project/modal.js
+++ Modal_Project/modal.js
@@ -1,11 +1,8 @@
 var myModal = document.getElementById("myModal");
+var slides = document.getElementsByClassName("modal-content");
 
 function openModal(){
 	myModal.style.display = "block";
-}
-
-function currentSlide(n){
-
 }
 
 function closeModal(){
@@ -17,4 +14,13 @@ window.onclick = function(event){
 	{
 		myModal.style.display = "none";
 	}
+}
+
+function showSlides(n){
+	var i;
+    for(i=0; i<slides.length; i++){
+        slides[i].style.display = "none";
+    }
+
+    slides[n].style.display = "block";
 }
