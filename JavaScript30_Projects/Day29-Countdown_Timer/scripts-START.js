4b00a7ca85ac408541fd481d421bdc91881724a5
gauravk268 <gauravk26800@gmail.com>
2021-01-12 23:59:41 +0530

    completed day29 project of JavaScript30

--- JavaScript30_Projects/Day29-Countdown_Timer/scripts-START.js
+++ JavaScript30_Projects/Day29-Countdown_Timer/scripts-START.js
@@ -1,8 +1,11 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds){
+  clearInterval(countdown);
+
   const now = Date.now();
   const then = now + seconds*1000;
   // console.log({now, then});
@@ -27,12 +30,29 @@ function displayTimeLeft(seconds){
   const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
   document.title =display;
   timerDisplay.textContent = display;
-  console.log({minutes, remainderSeconds});
+  // console.log({minutes, remainderSeconds});
 }
 
 function displayEndTime(timerStamp){
-  const end = new Date(timestamp);
+  const end = new Date(timerStamp);
   const hour = end.getHours();
   const minutes = end.getMinutes();
-  endTime.textContent = `Be Back Ar ${hour > 12 ? hour-12 : hour}:${minutes < 0? '0':''}${minutes}`;
+  endTime.textContent = `Be Back At
+  
+  ${hour > 12 ? hour-12 : hour}:${minutes < 0? '0':''}${minutes}`;
 }
+
+function startTimer(){
+  // console.log(this.dataset.time);
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function(e){
+  e.preventDefault();
+  const mins = this.minutes.value;
+  console.log(mins);
+  timer(mins*60);
+  this.reset();
+})
